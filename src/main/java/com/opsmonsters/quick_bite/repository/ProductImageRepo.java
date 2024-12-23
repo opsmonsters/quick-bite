4a6eaa91b96b90de0a3a9320f80a5800d2fc18cf
VIPIN <vipin.p@opsmonsters.in>
2024-12-23 19:47:43 +0530

    Added ProductImageRepo and related files

--- src/main/java/com/opsmonsters/quick_bite/repository/ProductImageRepo.java
+++ src/main/java/com/opsmonsters/quick_bite/repository/ProductImageRepo.java
@@ -1,4 +1,10 @@
 package com.opsmonsters.quick_bite.repository;
 
-public interface ProductImageRepo {
+import com.opsmonsters.quick_bite.model.ProductImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductImageRepo extends JpaRepository<ProductImage, Long> {
+
 }
