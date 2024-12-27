9d8fcd6187e384cbcd2ea1feaef4b5e0c10e4a6f
VIPIN <vipin.p@opsmonsters.in>
2024-12-27 11:46:44 +0530

    Created GET, POST, PUT, DELETE for Product Entity

---
+++ src/main/java/com/opsmonsters/quick_bite/controller/ProductController.java
@@ -0,0 +1,48 @@
+package com.opsmonsters.quick_bite.controller;
+
+import com.opsmonsters.quick_bite.dto.ProductDto;
+import com.opsmonsters.quick_bite.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @PostMapping
+    public ResponseEntity<String> createProduct(@RequestBody ProductDto dto) {
+        productService.createProduct(dto);
+        return new ResponseEntity<>("Product posted successfully", HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProductDto>> getAllProducts() {
+        List<ProductDto> products = productService.getAllProducts();
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
+
+    @GetMapping("/{productId}")
+    public ResponseEntity<ProductDto> getProductById(@PathVariable("productId") Long productId) {
+        ProductDto product = productService.getProductById(productId);
+        return new ResponseEntity<>(product, HttpStatus.OK);
+    }
+
+    @PutMapping("/{productId}")
+    public ResponseEntity<String> updateProduct(@PathVariable("productId") Long productId, @RequestBody ProductDto dto) {
+        productService.updateProduct(productId, dto);
+        return new ResponseEntity<>("Product updated successfully", HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{productId}")
+    public ResponseEntity<String> deleteProduct(@PathVariable("productId") Long productId) {
+        productService.deleteProduct(productId);
+        return new ResponseEntity<>("Product deleted successfully", HttpStatus.NO_CONTENT);
+    }
+}
